a764f847c8a706dd358cf89aff1ebfe99b9585e3
ousmane <sangaryousmane@gmail.com>
2024-07-01 20:28:58 +0800

    Add more components: Navbar, Menu, and Teacher mode functionalities

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalysticsPage = () => {
+    return ( 
+        <div>
+            Analytics Page
+        </div>
+     );
+}
+ 
+export default AnalysticsPage;
---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+
+
+const TestPage = () => {
+    return ( 
+       <div>
+          This is a test page
+       </div>
+    );
+}
+
+export default TestPage;
