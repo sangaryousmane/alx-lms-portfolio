fc439648abe2f8a9cf8376c07aaaeb999fefaa46
ousmane <sangaryousmane@gmail.com>
2024-07-04 13:01:29 +0800

    Modify main app - entry point of the application. Add additional route

--- Educational-Website-Frontend-React-main/src/App.js
+++ Educational-Website-Frontend-React-main/src/App.js
@@ -9,6 +9,7 @@ import Blog from "./components/blog/Blog";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/common/footer/Footer";
 import Home from "./components/home/Home";
+
 function App() {
   return (
     <>
