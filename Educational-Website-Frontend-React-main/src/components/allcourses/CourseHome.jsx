2cff386c7e7093f7dc3ad962d171f40dd7431fe9
ousmane <sangaryousmane@gmail.com>
2024-07-04 13:00:16 +0800

    Complete course card

--- Educational-Website-Frontend-React-main/src/components/allcourses/CourseHome.jsx
+++ Educational-Website-Frontend-React-main/src/components/allcourses/CourseHome.jsx
@@ -6,7 +6,7 @@ import OnlineCourses from "./OnlineCourses"
 const CourseHome = () => {
   return (
     <>
-      <Back title='Explore Courses' />
+      <Back title='Explore our top Courses' />
       <CoursesCard />
       <OnlineCourses />
     </>
